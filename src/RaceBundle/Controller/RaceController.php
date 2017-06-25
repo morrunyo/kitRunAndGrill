5bc43d5ad0052b8a7f2aabad11c0b55b4e6d93f5
Mateo Soto <oncekilosymedioesuna@gmail.com>
2017-06-25 20:05:44 +0000

    Jugando a activar carreras

--- src/RaceBundle/Controller/RaceController.php
+++ src/RaceBundle/Controller/RaceController.php
@@ -3,11 +3,30 @@
 namespace RaceBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use RaceBundle\Entity\Race;
 
 class RaceController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('RaceBundle:Default:index.html.twig');
+        $races = $this->getDoctrine()->getRepository('RaceBundle:Race')->findAll();
+        return $this->render('RaceBundle:Race:index.html.twig',$arrayName = array('races' => $races));
+    }
+    
+    public function activateAction($id)
+    {
+        $races = $this->getDoctrine()->getRepository('RaceBundle:Race')->findAll();
+        foreach ($races as $race)
+        {
+            if ($race->getIsActive())
+            {
+                $race->setIsActive(false);
+            }
+            if ($race->getId()==$id)
+            {   
+                $race->setIsActive(true);
+            }
+        }
+        return $this->render('RaceBundle:Race:index.html.twig',$arrayName = array('races' => $races));
     }
 }
